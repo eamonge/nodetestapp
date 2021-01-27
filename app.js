3a4349bbebd7296c048b8846124c6f3dfad57a2c
eamonge <39276293+eamonge@users.noreply.github.com>
2021-01-27 15:43:05 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+
+
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+  });
